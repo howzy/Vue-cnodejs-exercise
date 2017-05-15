28b4a8f230c883b2a1c5eae639f4f756fc5ddf8c
zh-once <1007552066@qq.com>
2017-05-16 00:44:19 +0800

    详情页title部分

--- src/pages/topic.vue
+++ src/pages/topic.vue
@@ -1,58 +1,68 @@
 <template>
   <div>
     <nv-head page-type="主题" :fix-head="true"></nv-head>
-    <div id="page">
+    <div id="page" v-if="topic.title">
       <h2 class="topic-title">{{topic.title}}</h2>
-      section
+      <section class="author-info">
+        <img class="avatar" :src="topic.author.avatar_url" />
+        <div class="col">
+          <span>{{topic.author.loginname}}</span>
+          <time>
+            发布于:{{topic.create_at}}
+          </time>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
-import $ from 'webpack-zepto'
-import nvHead from '../components/header';
+  import $ from 'webpack-zepto'
+  import nvHead from '../components/header';
 
-export default {
-  data() {
-    return {
-      showMenu: false, // 是否展示左侧菜单栏
-      topic: {}, // 主题
-      topicId: '',
-      noData: false
-    }
-  },
-  mounted() {
-    // 从路由中获取主题id
-    this.topicId = this.$route.params.id;
-    // 根据主题id加载相应的主题数据
-    $.get('https://cnodejs.org/api/v1/topic/' + this.topicId, res => {
-      if (res && res.data) {
-        this.topic = res.data;
-      } else {
-        this.noData = true;
+  export default {
+    data() {
+      return {
+        showMenu: false, // 是否展示左侧菜单栏
+        topic: {}, // 主题
+        topicId: '',
+        noData: false
       }
-    })
-  },
-  components: {
-    nvHead
+    },
+    mounted() {
+      // 从路由中获取主题id
+      this.topicId = this.$route.params.id;
+      // 根据主题id加载相应的主题数据
+      $.get('https://cnodejs.org/api/v1/topic/' + this.topicId, res => {
+        if (res && res.data) {
+          this.topic = res.data;
+        } else {
+          this.noData = true;
+        }
+      })
+    },
+    components: {
+      nvHead
+    }
   }
-}
+
 </script>
 
 <style lang="scss">
-#page {
-  padding-top: 44px;
-  background-color:#fff;
-  transition: all .3s ease;
-  overflow-x:hidden;
-}
-.topic-title {
-  margin: 15px;
-  padding: 5px;
-  font-size: 18px;
-  color: #2c3e50;
-  line-height: 1.5;
-  background-color: #f0f0f0;
-  border-radius: 5px;
-}
+  #page {
+    padding-top: 44px;
+    background-color: #fff;
+    transition: all .3s ease;
+    overflow-x: hidden;
+  }
+  
+  .topic-title {
+    margin: 15px;
+    padding: 5px;
+    font-size: 18px;
+    color: #2c3e50;
+    line-height: 1.5;
+    background-color: #f0f0f0;
+    border-radius: 5px;
+  }
 </style>
